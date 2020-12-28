aa8b2b3cd6c7589ef98ffb2f3a570f040e356c03
Paulo <paulokingdev@gmail.com>
2020-12-28 14:38:30 -0800

    paulo v1.0

--- components/provider/DeviceProvider.js
+++ components/provider/DeviceProvider.js
@@ -13,6 +13,10 @@ const DeviceProvider = (props) => {
   const [appState, setAppState] = useState(AppState.currentState);
   const [peripherals, setPeripherals] = useState( new Map() );
   const [valueState, setValueState] = useState('');
+  const [nameState, setNameState] = useState('');
+  const [profileState, setProfileState] = useState('');
+  const [serviceState, setServiceState] = useState('');
+  const [characteristicState, setCharacteristicState] = useState('');
 
   return (
     //<DeviceContext.Provider value={ {deviceID, setDeviceID}}>
@@ -22,6 +26,10 @@ const DeviceProvider = (props) => {
       value3: [appState, setAppState],
       value4: [peripherals, setPeripherals],
       value5: [valueState, setValueState],
+      value6: [nameState, setNameState],
+      value7: [profileState, setProfileState],
+      value8: [serviceState, setServiceState],
+      value9: [characteristicState, setCharacteristicState],
     }}>
       {props.children}
     </DeviceContext.Provider>
